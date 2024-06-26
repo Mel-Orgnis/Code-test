2e658ec85f6315dd306c2f4f387495b00de0ecbb
woosuk <giuseog@gmail.com>
2024-06-26 14:08:38 +0900

    commit: 전화번호 목록 주석 추가

--- Code_by_woosuk/프로그래머스/42577. 전화번호 목록/전화번호 목록.cpp
+++ Code_by_woosuk/프로그래머스/42577. 전화번호 목록/전화번호 목록.cpp
@@ -6,10 +6,11 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
+    //사전순 정렬
     sort(phone_book.begin(), phone_book.end());
     
     for(int i=0; i<phone_book.size()-1; i++){
-    if(phone_book[i+1].find(phone_book[i]) == 0)
+    if(phone_book[i+1].find(phone_book[i]) == 0) //인덱스 0부터 phone_book[i+1]는 index 0부터 phone_book[i]이 일치할 때.
         return false;
 }
     return true;
